d21a897e119eec3dee0d1ccfa07720036dfa0a92
Igor <igor.borowski@capgemini.com>
2022-08-04 14:00:41 +0200

    add profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TokenStorageService } from '../_services/token-storage.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
+  currentUser: any;
+  constructor(private token: TokenStorageService) { }
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
   }
-
 }
