a0b1676f349369b37202ded2659ee21f9631104e
Igor <igor.borowski@capgemini.com>
2022-08-04 14:13:58 +0200

    config board admin/mod/user components

--- src/app/board-moderator/board-moderator.component.ts
+++ src/app/board-moderator/board-moderator.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../_services/user.service';
 @Component({
-  selector: 'app-board-moderator',
+  selector: 'app-board-admin',
   templateUrl: './board-moderator.component.html',
-  styleUrls: ['./board-moderator.component.scss']
+  styleUrls: ['./board-moderator.component.css']
 })
-export class BoardModeratorComponent implements OnInit {
-
-  constructor() { }
-
+export class BoardAdminComponent implements OnInit {
+  content?: string;
+  constructor(private userService: UserService) { }
   ngOnInit(): void {
+    this.userService.getModeratorBoard().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    });
   }
-
 }
--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../_services/user.service';
 @Component({
-  selector: 'app-board-user',
+  selector: 'app-board-admin',
   templateUrl: './board-user.component.html',
-  styleUrls: ['./board-user.component.scss']
+  styleUrls: ['./board-user.component.css']
 })
-export class BoardUserComponent implements OnInit {
-
-  constructor() { }
-
+export class BoardAdminComponent implements OnInit {
+  content?: string;
+  constructor(private userService: UserService) { }
   ngOnInit(): void {
+    this.userService.getUserBoard().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    });
   }
-
 }
